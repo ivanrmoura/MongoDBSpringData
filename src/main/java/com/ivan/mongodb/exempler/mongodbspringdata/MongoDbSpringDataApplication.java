f8e9c8d840aa1fa0fb69b11c74bbe49338a4b5dc
Ivan Rodrigues <ivan.rodrigues@lsdi.ufma.br>
2022-02-24 13:00:11 -0300

    primeiro commit

---
+++ src/main/java/com/ivan/mongodb/exempler/mongodbspringdata/MongoDbSpringDataApplication.java
@@ -0,0 +1,13 @@
+package com.ivan.mongodb.exempler.mongodbspringdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbSpringDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoDbSpringDataApplication.class, args);
+    }
+
+}
